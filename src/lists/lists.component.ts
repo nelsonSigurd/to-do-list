3c5d4c5e45ce650af720044aef9dafb95c584b65
nelsonSigurd <nellysig@me.com>
2024-07-14 19:55:06 -0500

    Start setting up list component

--- src/lists/lists.component.ts
+++ src/lists/lists.component.ts
@@ -1,12 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+interface list {
+  id: number;
+  title: string;
+  cards: any[];
+}
 
 @Component({
   selector: 'app-lists',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './lists.component.html',
   styleUrl: './lists.component.css'
 })
 export class ListsComponent {
+  lists: list[] = []; 
+  title: string = "";
 
+  addList(title:string) {
+    const newList:list = {id:this.lists.length, title, cards:[]}
+    this.lists.push(newList);
+    this.title="";
+  }
 }
